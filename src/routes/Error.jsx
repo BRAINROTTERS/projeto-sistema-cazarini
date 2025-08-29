9fc75725da72e2fb7e2fe884ba1cfe735f5bf8a2
joao <jvcazarini@gmail.com>
2025-08-29 09:28:57 -0300

    criando as abas

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export const Error = () => {
+const Error = () => {
   return (
-    <div>Error</div>
+    <div>404- Page not found</div>
   )
 }
+
+export default Error
